6c106f891f0e22e51e468d15b3cdc0b9d2e81cef
Zac Wasielewski <zac@wasielewski.org>
2014-03-27 16:19:27 -0400

    Load custom Google webfonts: Merriweather and Montserrat

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,9 @@
 /**
  * Custom functions
  */
+
+function load_fonts() {
+  wp_enqueue_style( 'google-fonts','//fonts.googleapis.com/css?family=Montserrat|Merriweather');
+}
+
+add_action('wp_enqueue_scripts', 'load_fonts');
