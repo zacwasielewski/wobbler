a34701b0facbd6a1fa7f941c232f8c809a354230
Zac Wasielewski <zac@wasielewski.org>
2014-03-28 15:13:20 -0400

    Base text styling

--- lib/custom.php
+++ lib/custom.php
@@ -4,7 +4,8 @@
  */
 
 function load_fonts() {
-  wp_enqueue_style( 'google-fonts','//fonts.googleapis.com/css?family=Montserrat|Merriweather');
+  wp_enqueue_style( 'google-fonts','//fonts.googleapis.com/css?family=Lora');
 }
-
 add_action('wp_enqueue_scripts', 'load_fonts');
+
+add_image_size( 'header', 1280, 9999 );
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,14 +1,23 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+      <div class="entry-thumbnail">
+        <?php the_post_thumbnail( 'header' ); ?>
+      </div>
+      <div class="container">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php get_template_part('templates/entry-meta'); ?>
+      </div>
     </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
+    <div class="container">
+      <div class="entry-content">
+        <?php the_content(); ?>
+      </div>
     </div>
     <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+      <div class="container">
+        <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+      </div>
     </footer>
     <?php comments_template('/templates/comments.php'); ?>
   </article>
