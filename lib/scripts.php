db4f8d26fbe0248b942288289de782fb1f3fa44b
Zac Wasielewski <zac@wasielewski.org>
2014-03-29 20:00:43 -0400

    Increase footer padding

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c299934fbd3448556e3112ae3fcb0efa');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '03e04d7df4a19039129755e9b41b80ba');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
