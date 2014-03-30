83554376e3da796e4a44c15185b00dfd2151c45d
Zac Wasielewski <zac@wasielewski.org>
2014-03-29 20:04:58 -0400

    Improve thumbnail caption padding

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '03e04d7df4a19039129755e9b41b80ba');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '633d6b339cb45074b9d174e5bff4784f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
