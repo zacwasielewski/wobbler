9a3693a9d3a24c66e792a1c34abfdbb46cd049f1
Zac Wasielewski <zac@wasielewski.org>
2014-03-31 16:39:12 -0400

    Reduce margin-bottom of .thumbnail elements

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c37c1cfc86ee5d236a582a87e41276fe');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7b2b45c9b053aa22269d6f1a35077e1b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
